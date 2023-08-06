5b7e3148f03f70ccc1fd06c4aa08b79e5382147c
Oleksandr Bron <lexlutermotion@gmail.com>
2023-08-06 15:06:19 +0300

    swr added

--- components/Posts.tsx
+++ components/Posts.tsx
@@ -1,20 +1,23 @@
 "use client";
-import { usePosts } from "@/store";
-import { shallow } from "zustand/shallow";
-import { useEffect } from "react";
+import useSWR from "swr";
+// import { usePosts } from "@/store";
+// import { shallow } from "zustand/shallow";
+// import { useEffect } from "react";
 import Link from "next/link";
+import { getAllData } from "@/services/getPosts";
 
 export const Posts = () => {
-  const [posts, loading, getAllPosts] = usePosts(
-    (state) => [state.posts, state.loading, state.getAllPosts],
-    shallow
-  );
+  const { data: posts, isLoading } = useSWR("posts", getAllData);
+  // const [posts, loading, getAllPosts] = usePosts(
+  //   (state) => [state.posts, state.loading, state.getAllPosts],
+  //   shallow
+  // );
 
-  useEffect(() => {
-    getAllPosts();
-  }, [getAllPosts]);
+  // useEffect(() => {
+  //   getAllPosts();
+  // }, [getAllPosts]);
 
-  return loading ? (
+  return isLoading ? (
     <h3>loading...</h3>
   ) : (
     <ul className="postsList">
